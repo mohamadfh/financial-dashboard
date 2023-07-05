0010212252fb2597be56d4c30633e0417e994362
Mohammad Fallah <fallahmohammadm@gmail.com>
2023-07-05 10:50:38 +0330

    set interval to update and show df

--- app.py
+++ app.py
@@ -1,19 +1,63 @@
+
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 import random
+import time
 import datetime
+import data 
+import hermes
+import threading
+
+
+data.set_interval(data.update_df,10)
+
+# # Create an empty DataFrame to store the scatter plot data
+# df = pd.DataFrame(columns=['index', 'time'])
 
 # Create a Dash app
 app = dash.Dash(__name__)
 
 # Define the app layout
 app.layout = html.Div([
-    "Total Index:"
+    dcc.Graph(id='scatter-plot'),
+    
+    dcc.Interval(
+        id='interval-component',
+        interval=5 * 1000,  # Update every 10 seconds (in milliseconds)
+        n_intervals=0
+    )
 ])
 
+# Update the scatter plot every 10 seconds
+@app.callback(
+    Output('scatter-plot', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def update_scatter_plot(n):
+    # time = datetime.datetime.now().strftime('%H:%M:%S')
+    # stock_df = hermes.Market_with_askbid()
+    # ti = data.calc_total_index(stock_df)
+    # new_data = {'time': time, 'index': ti}
+    # print(time)
+    # print(ti)
+    # df.loc[len(df)] = new_data
+
+    # Create the scatter plot
+    fig = go.Figure(data=go.Scatter(
+        x=data.df['time'],
+        y=data.df['index'],
+    ))
+    fig.update_xaxes(
+    rangeslider_visible=True,
+    tickformatstops = [
+        dict(dtickrange=[None, 60000], value="%H:%M:%S s"),
+        dict(dtickrange=[60000, 3600000], value="%H:%M m"),
+        dict(dtickrange=[3600000, 86400000], value="%H:%M h")
+    ])
+    return fig
 
 # Run the app
 if __name__ == '__main__':
--- data.py
+++ data.py
@@ -2,18 +2,38 @@ import pandas as pd
 import numpy as np
 from hermes import Market_with_askbid
 from typing import Union
+import threading
+import time 
+import datetime
+import hermes
+df = pd.DataFrame(columns=['index', 'time'])
 
-def calc_index(df: pd.DataFrame) -> float:
+def update_df():
+    global df
+    time = datetime.datetime.now().strftime('%H:%M:%S')
+    stock_df = hermes.Market_with_askbid()
+    ti = calc_total_index(stock_df)
+    new_data = {'time': time, 'index': ti}
+    df.loc[len(df)] = new_data
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec) 
+        func()  
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+def calc_total_index(df: pd.DataFrame) -> float:
     last_return = df['last_return'].astype(float)
     value = df['value'].astype(float)
-    weighted_avg = (last_return * value ).sum() / value.sum()
+    weighted_avg = (last_return * value).sum() / value.sum()
     return weighted_avg
 
 def filter_funds(df: pd.DataFrame) -> pd.DataFrame:
     return df[df['isin'].str.startswith('IRT')]
 
-if __name__ == "__main__":
-    df = Market_with_askbid()
-    print("fetched")
-    print(calc_index(df))
-    print(calc_index(filter_funds(df)))
+def calc_funds_index(df: pd.DataFrame) -> float:
+    return calc_total_index(filter_funds(df))
+
+
